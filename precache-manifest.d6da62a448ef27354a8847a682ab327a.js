7a38f39de007e4295427db3d17984534f58400ac
kezben <kezbennetts@gmail.com>
2020-09-25 11:25:18 +1200

    Updates

---
+++ precache-manifest.d6da62a448ef27354a8847a682ab327a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017a65f619ad6c795e1762fbedcff3b5",
+    "url": "/Harry-Potter-quiz/index.html"
+  },
+  {
+    "revision": "902bf370edbd52fde16c",
+    "url": "/Harry-Potter-quiz/static/css/main.d38c7e30.chunk.css"
+  },
+  {
+    "revision": "a82d810bc73883cef512",
+    "url": "/Harry-Potter-quiz/static/js/2.d9e1e8ce.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Harry-Potter-quiz/static/js/2.d9e1e8ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902bf370edbd52fde16c",
+    "url": "/Harry-Potter-quiz/static/js/main.0b8ff1fc.chunk.js"
+  },
+  {
+    "revision": "f256375119031175e6b5",
+    "url": "/Harry-Potter-quiz/static/js/runtime-main.f351790f.js"
+  }
+]);
